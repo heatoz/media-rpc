50c5003d582943ea07996be64889bfbefa161684
heatoz <kinonatsu27@gmail.com>
2026-02-23 16:51:34 -0300

    feat(infra): add aiohttp ClientSession interface

---
+++ src/mpcrpc/infra/http_client.py
@@ -0,0 +1,79 @@
+import aiohttp
+
+class HttpClient:
+	"""
+	Asynchronous HTTP client interface based on the requests library.
+	"""
+
+	def __init__(self, headers: dict | None = None, cookies: dict | None = None) -> None:
+		"""
+		Initialize an HttpClient instance.
+
+		Args:
+			headers (dict, optional):
+				Optional HTTP headers to include in
+				all requests. Defaults to None.
+
+			cookies (Cookies, optional):
+				Optional Cookies object to include
+				in all requests. Defaults to None.
+
+		Attributes:
+			session (requests.Session):
+				A persistent requests session object
+				used for all HTTP requests.
+		"""
+
+		self.session: aiohttp.ClientSession = aiohttp.ClientSession(
+			headers = headers,
+			cookies = cookies
+		)
+
+	async def get(self, path: str) -> str:
+		"""
+		Perform a synchronous HTTP GET request.
+
+		Args:
+			path (str):
+				The URL to send the GET request to.
+
+		Returns:
+			requests.Response:
+				The Response object from the GET request.
+		"""
+
+		r: aiohttp.ClientResponse = await self.session.get(
+			path
+		)
+
+		return await r.text(encoding="utf-8")
+
+	async def post(self, path: str, data: dict) -> str:
+		"""
+		Perform a synchronous HTTP POST request with JSON-encoded data.
+
+		Args:
+			path (str):
+				The URL to send the POST request to.
+
+			data (dict):
+				The payload to send as JSON in the POST body.
+
+		Returns:
+			requests.Response:
+				The Response object from the POST request.
+		"""
+
+		r: aiohttp.ClientResponse = await self.session.post(
+			path,
+			data=data
+		)
+
+		return await r.text(encoding="utf-8")
+
+	async def close(self) -> None:
+		"""
+		Close the underlying requests session.
+		"""
+
+		await self.session.close()
