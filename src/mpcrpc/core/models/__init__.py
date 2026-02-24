1f30b1593aab8d9a3de5ba948900b488e681c40c
heatoz <kinonatsu27@gmail.com>
2026-02-24 20:49:47 -0300

    chore: fix tabbing

---
+++ src/mpcrpc/core/models/__init__.py
@@ -0,0 +1,3 @@
+from .mpc import PlaybackSession
+
+__all__ = ["PlaybackSession"]
--- src/mpcrpc/utils/cache.py
+++ src/mpcrpc/utils/cache.py
@@ -1,44 +1,44 @@
 from typing import Any
 
 class Cache:
-    """
-    Basic in-memory cache without expiration policy.
+	"""
+	Basic in-memory cache without expiration policy.
 
-    Created to keep project boundaries well delimited and preserve modularity.
-    """
+	Created to keep project boundaries well delimited and preserve modularity.
+	"""
 
-    def put(self, key: str, value: Any) -> None:
-        """
-        Store a value in the cache under the given key.
+	def put(self, key: str, value: Any) -> None:
+		"""
+		Store a value in the cache under the given key.
 
-        The value is stored as an attribute of the Cache instance.
+		The value is stored as an attribute of the Cache instance.
 
-        Args:
-            key (str):
-                The key under which the value will be stored.
-                Must be a valid Python attribute name.
-                
-            value (Any):
-                The value to be cached.
-        """
+		Args:
+			key (str):
+				The key under which the value will be stored.
+				Must be a valid Python attribute name.
+				
+			value (Any):
+				The value to be cached.
+		"""
 
-        return setattr(self, key, value)
+		return setattr(self, key, value)
 
-    def get(self, key: str) -> Any:
-        """
-        Retrieve a value from the cache by its key.
+	def get(self, key: str) -> Any:
+		"""
+		Retrieve a value from the cache by its key.
 
-        Args:
-            key (str):
-                The key associated with the cached value.
+		Args:
+			key (str):
+				The key associated with the cached value.
 
-        Returns:
-            Any:
-                The cached value associated with the given key.
+		Returns:
+			Any:
+				The cached value associated with the given key.
 
-        Raises:
-            AttributeError:
-                If the key does not exist in the cache.
-        """
+		Raises:
+			AttributeError:
+				If the key does not exist in the cache.
+		"""
 
-        return getattr(self, key)
+		return getattr(self, key)
