9959a495e589a763ed60a687d54ce1b7b497fb98
heatoz <kinonatsu27@gmail.com>
2026-02-25 13:47:01 -0300

    chore(infra): remove Config from infra __init__

--- src/mpcrpc/infra/__init__.py
+++ src/mpcrpc/infra/__init__.py
@@ -1,5 +1,4 @@
 from .http_client import HttpClient
 from .event_bus import EventBus
-from .config import Config
 
-__all__ = ["HttpClient", "EventBus", "Config"]
+__all__ = ["HttpClient", "EventBus"]
