5d4b25f3b21d073e69c73cbb0692f6e2dc8f49c2
heatoz <kinonatsu27@gmail.com>
2026-02-25 10:58:28 -0300

    feat(infra): add Config to __init__

--- src/mpcrpc/infra/__init__.py
+++ src/mpcrpc/infra/__init__.py
@@ -1,4 +1,5 @@
 from .http_client import HttpClient
 from .event_bus import EventBus
+from .config import Config
 
-__all__ = ["HttpClient", "EventBus"]
+__all__ = ["HttpClient", "EventBus", "Config"]
