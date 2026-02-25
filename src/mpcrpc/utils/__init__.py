91ab8ed776c3240a2c2957c7c7b1f0409b501229
heatoz <kinonatsu27@gmail.com>
2026-02-25 13:47:48 -0300

    chore(utils): add Config to __init__

--- src/mpcrpc/utils/__init__.py
+++ src/mpcrpc/utils/__init__.py
@@ -1,4 +1,5 @@
 from .parser import Regex, Filename
+from .config import Config
 from .cache import Cache
 
-__all__ = ["Regex", "Filename", "Cache"]
+__all__ = ["Regex", "Filename", "Config", "Cache"]
