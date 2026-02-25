5c006c7947290aad5bb1e0c893396f02900f3c08
heatoz <kinonatsu27@gmail.com>
2026-02-24 21:45:22 -0300

    feat(services): make MPC service publish events and implement its cache usage

--- src/mpcrpc/core/services/mpc.py
+++ src/mpcrpc/core/services/mpc.py
@@ -1,12 +1,14 @@
-from mpcrpc.core.models.mpc import PlaybackSession
-from mpcrpc.infra import HttpClient
+from mpcrpc.core.events import PlaybackSessionUpdated, PlaybackFileUpdated
+from mpcrpc.infra import HttpClient, EventBus, Parser, Cache
+from mpcrpc.core.models import PlaybackSession, PlaybackFile
+from typing import Any
 
 class MPC:
 	"""
 	Client interface for interacting with an MPC HTTP instance.
 	"""
 
-	def __init__(self, port: int) -> None:
+	def __init__(self, event_bus: EventBus, port: int = 13579) -> None:
 		"""
 		Initialize the MPC client.
 
@@ -16,19 +18,47 @@ class MPC:
 		"""
 
 		self._client: HttpClient = HttpClient()
+		self._cache: Cache = Cache()
+		self._event_bus = event_bus
+
 		self.port: int = port
 	
-	async def Variables(self) -> PlaybackSession:
+	async def Variables(self) -> None:
 		"""
 		Fetch current playback session from MPC variables endpoint.
-
-		Returns:
-			PlaybackSession:
-				Parsed playback session data.
 		"""
 
 		response: str = await self._client.get(
 			f"localhost:{self.port}/variables.html"
 		)
 
-		return PlaybackSession(response)
+		p_data: dict = Parser.Variables(response)
+
+		p_session: PlaybackSession = PlaybackSession(p_data)
+		p_file: PlaybackFile = PlaybackFile(p_data)
+
+		c_session: Any = self._cache.get("c_session")
+		c_file: Any = self._cache.get("c_file")
+
+		# checks if a cached PlaybackSession exists
+		# and if it exists, check if the current
+		# is equal to the cached one.
+		if not c_session or c_session != p_session:
+			
+			# idk of the atomicity of this, too lazy to test :p
+			self._cache.put("c_session", p_session)
+
+			return await self._event_bus.publish(
+				PlaybackSessionUpdated(p_session)
+			)
+
+		# checks if a cached PlaybackFile exists
+		# and if it exists, check if the current
+		# is equal to the cached one.
+		if not c_file or c_file != p_file:
+
+			self._cache.put("c_file", p_file)
+
+			return await self._event_bus.publish(
+				PlaybackFileUpdated(p_file)
+			)
