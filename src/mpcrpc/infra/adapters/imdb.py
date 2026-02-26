89975f70ba9e6d53830a0438ff7256ceac3c433f
heatoz <kinonatsu27@gmail.com>
2026-02-26 10:54:49 -0300

    feat(adapters): add imdb query method

--- src/mpcrpc/infra/adapters/imdb.py
+++ src/mpcrpc/infra/adapters/imdb.py
@@ -1,16 +1,36 @@
 from mpcrpc.infra import HttpClient
 import urllib.parse, json
+from typing import Any
 
-class Imdb:
+class IMDB:
 	"""
-	Wrapper around some IMDB API methods.
+	Adapter for the IMDB API methods.
 	"""
 
-	# GET https://api.imdbapi.dev/search/titles?query=
-	@staticmethod
-	async def Titles(name: str) -> str | None:
+	def __init__(self):
 		"""
-		Searches a title from IMDB using the Titles endpoint.
+		Initialize a IMDB adapter object.
+
+		Attributes:
+			client (HttpClient):
+				Class HTTP Session for requests.
+		"""
+
+		self._client: HttpClient = HttpClient(
+			headers = {
+				"Accept": "application/json"
+			}
+		)
+
+	async def Search(self, name: str) -> str | None:
+		"""
+		Searches a title from IMDB.
+
+		GET https://api.imdbapi.dev/search/titles?query=
+
+		Note:
+			Currently using api.imdbapi.dev for the requests,
+			maybe i'll change this on future.
 
 		Args:
 			name (str):
@@ -18,16 +38,53 @@ class Imdb:
 
 		Returns:
 			str:
-				The id of the first result from the query.
+				The id of the first result from the search.
 			None:
 				If the query returns no results.
 		"""
 
-		client: HttpClient = HttpClient()
-		
-		response: str = await client.get(
-			f"https://api.imdbapi.dev/search/titles?query={urllib.parse.quote(name)}"
+		response: str = await self._client.get(
+			f"https://api.imdbapi.dev/search/titles?query={urllib.parse.quote(name)}&limit=1"
 		)
 
-		return json.loads(response).get("titles")[0]
+		return json.loads(response).get("titles")[0].get("id")
 	
+	async def Query(self, mid: str) -> dict[str, str]:
+		"""
+		Queries a title details from its id.
+
+		GET https://api.imdbapi.dev/titles/
+
+		Note:
+			Currently using api.imdbapi.dev for the requests,
+			maybe i'll change this on future.
+
+		Args:
+			mid (str):
+				The media id.
+		
+		Returns:
+			dict:
+				Contains
+		"""
+
+		response: str = await self._client.get(
+			f"https://api.imdbapi.dev/titles/{mid}"
+		)
+
+		j_resp: Any = json.loads(response)
+
+		# idk if this is just a bloat but made this
+		# to keep a good standard between all
+		# adapters return values.
+		# Note: didn't do a replacement on movies also
+		# because their type, on IMDB, already comes as 'movie'.
+		_type = j_resp.get("type").replace("tvSeries", "series")
+
+		return {
+			"director": j_resp.get("directors")[0].get("displayName"),
+			"poster": j_resp.get("primaryImage").get("url"),
+			"title": j_resp.get("primaryTitle"),
+			"year": j_resp.get("startYear"),
+			"type": _type
+		}
