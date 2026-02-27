e8495e02c54dc1512006284398152e068819408e
heatoz <kinonatsu27@gmail.com>
2026-02-27 18:47:29 -0300

    fix(adapters): rename types to models on __init__

--- src/mpcrpc/infra/adapters/__init__.py
+++ src/mpcrpc/infra/adapters/__init__.py
@@ -1,4 +1,5 @@
+from .models import SearchResult, QueryResult
 from .imdb import IMDB
 from .tmdb import TMDB
 
-__all__ = ["IMDB", "TMDB"]
+__all__ = ["SearchResult", "QueryResult", "IMDB", "TMDB"]
