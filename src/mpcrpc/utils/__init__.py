7cc6a8535248b512e5597a542af05b5d0cc0f664
heatoz <kinonatsu27@gmail.com>
2026-02-28 15:29:09 -0300

    chore(utils): remove config from __init__

--- src/mpcrpc/utils/__init__.py
+++ src/mpcrpc/utils/__init__.py
@@ -1,5 +1,4 @@
 from .parser import Regex, MediaFile
-from .config import Config
 from .cache import Cache
 
-__all__ = ["Regex", "MediaFile", "Config", "Cache"]
+__all__ = ["Regex", "MediaFile", "Cache"]
