390a6da29e77c4ac8569230b5d81a4c56fdaf9e4
heatoz <kinonatsu27@gmail.com>
2026-03-07 01:04:05 -0300

    fix(adapters): add episode_title to queryresult model

--- src/mpcrpc/infra/adapters/models.py
+++ src/mpcrpc/infra/adapters/models.py
@@ -22,13 +22,18 @@ class QueryResult:
     Represents the result of a Query operation.
     """
 
-    def __init__(self, director: str, poster: str, title: str, year: str) -> None:
+    def __init__(
+        self,
+        poster: str,
+        title: str,
+        year: str | None = None,
+        director: str | None = None,
+        episode_title: str | None = None
+        ) -> None:
         """
         Initialize a QueryResult object.
 
         Attributes:
-            director (str):
-                The media director.
 
             poster (str):
                 The media poster url.
@@ -36,11 +41,18 @@ class QueryResult:
             title (str):
                 The media title.
 
-            year (str):
-                The media release year.
+            year (optional, str):
+                The media (movie) release year.
+
+            director (optional, str):
+                The media (movie) director.
+
+            episode_title (optional, str):
+                The media (series) episode title.
         """
 
-        self.director: str = director
         self.poster: str = poster
         self.title: str = title
-        self.year: str = year
+        self.year: str | None = year
+        self.director: str | None = director
+        self.episode_title: str | None = episode_title
